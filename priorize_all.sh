03e2f165236f1ffc2b8f10562db43263d5443b79
PaulHancock <paul.hancock@curtin.edu.au>
2018-01-11 16:36:02 +0800

    check for input catalogues, fix typos

--- priorize_all.sh
+++ priorize_all.sh
@@ -2,14 +2,20 @@
 
 lowfreq=$(cat K2_154MHz.dat | awk '{print $1}')
 
-for f in lowfreq
-do
-    echo aegean ${f} --autoload --table ${f} --priorized 2 --input median_154MHz.fits
-done
+if [[ -e median_154MHz_comp.fits ]]
+then 
+    for f in ${lowfreq}
+    do
+	echo aegean ${f} --autoload --table ${f} --priorized 2 --input median_154MHz_comp.fits
+    done
+fi
 
-highfreq=$(cat K2_186MHz.dat | awk '{print $1}')
+highfreq=$(cat K2_185MHz.dat | awk '{print $1}')
 
-for f in highfreq
-do
-    echo aegean ${f} --autoload --table ${f} --priorized 2 --input median_154MHz.fits
-done
+if [[ -e median_185MHz_comp.fits ]]
+then
+    for f in ${highfreq}
+    do
+	echo aegean ${f} --autoload --table ${f} --priorized 2 --input median_185MHz_comp.fits
+    done
+fi
