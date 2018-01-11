d0567f8c69407c80ba76fb8d7842edac7d91da7f
PaulHancock <paul.hancock@curtin.edu.au>
2018-01-11 16:56:49 +0800

    refactor to use a single nested loop and use make

--- priorize_all.sh
+++ priorize_all.sh
@@ -1,21 +1,18 @@
 #! /usr/bin/env bash
 
-lowfreq=$(cat K2_154MHz.dat | awk '{print $1}')
+#
+# Make all the priorized catalogues for the 154 and 185 MHz images.
+#
 
-if [[ -e median_154MHz_comp.fits ]]
-then 
-    for f in ${lowfreq}
-    do
-	echo aegean ${f} --autoload --table ${f} --priorized 2 --input median_154MHz_comp.fits
-    done
-fi
-
-highfreq=$(cat K2_185MHz.dat | awk '{print $1}')
-
-if [[ -e median_185MHz_comp.fits ]]
-then
-    for f in ${highfreq}
-    do
-	echo aegean ${f} --autoload --table ${f} --priorized 2 --input median_185MHz_comp.fits
-    done
-fi
+for freq in 154 185
+do
+    files=$(cat K2_${freq}MHz.dat | awk '{print $1}')
+    if [[ -e median_${freq}MHz_comp.fits ]]
+    then
+	for f in ${files}
+	do
+	    make ${f%%.fits}_bkg.fits
+	    aegean ${f} --autoload --table ${f} --priorized 2 --input median_${freq}MHz_comp.fits
+	done
+    fi
+done
