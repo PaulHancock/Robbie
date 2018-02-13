87bde7ed4a439646018a4c5dac5790cafafe939c
PaulHancock <paul.hancock@curtin.edu.au>
2018-02-13 17:41:31 +0800

    actually submit jobs when run

--- galaxy_codes/warp_all.sh
+++ galaxy_codes/warp_all.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 
+field="K2"
+
 nimg=($( ls K2_trim_[0-9]*[0-9].fits | wc -l ))
 #nimg=${nimg[0]}
 
 echo ${field} ${nimg}
 
 sed s/NNN/${field}/g warp_all.template | sed s/NIMG/${nimg}/g > warp_${field}.sh
-echo "sbatch warp_${field}.sh"
+echo $( sbatch warp_${field}.sh)
