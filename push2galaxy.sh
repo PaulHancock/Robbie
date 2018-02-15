f39ed5ece9f0008745b25a430ee5b565a8e90a23
PaulHancock <paul.hancock@curtin.edu.au>
2018-02-15 12:05:42 +0800

    remove limit and copy all files

--- push2galaxy.sh
+++ push2galaxy.sh
@@ -3,5 +3,5 @@
 files=($( cat K2_1??MHz.dat | awk '{print $1}' ))
 xmfiles=($( cat K2_1??MHz.dat | awk '{print $1}' | sed 's/.fits/_xm.fits/g' ))
 
-rsync --ignore-existing --progress ${files[@]1::4} galaxy:/astro/mwasci/phancock/warping/.
-rsync --ignore-existing --progress ${xmfiles[@]1::4} galaxy:/astro/mwasci/phancock/warping/.
+rsync --ignore-existing --progress ${files[@]} galaxy:/astro/mwasci/phancock/warping/.
+rsync --ignore-existing --progress ${xmfiles[@]} galaxy:/astro/mwasci/phancock/warping/.
