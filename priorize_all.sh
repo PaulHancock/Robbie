82c380dd9e7ea0c15767b194fddf023878c8f52c
PaulHancock <paul.hancock@curtin.edu.au>
2018-02-16 10:00:04 +0800

    act on the warped images only

--- priorize_all.sh
+++ priorize_all.sh
@@ -6,17 +6,20 @@
 
 for freq in 154 185
 do
-    files=$(cat K2_${freq}MHz.dat | awk '{print $1}')
-    if [[ -e median_${freq}MHz_comp.fits ]]
-    then
-	for f in ${files}
-	do
-	    make ${f%%.fits}_bkg.fits
-	    table=${f%%.fits}_comp.fits
-	    if [[ ! -e ${table} ]]
-	    then
-		aegean ${f} --autoload --table ${f} --priorized 1 --input median_${freq}MHz_comp.fits --noregroup
-	    fi
-	done
-    fi
+    files=($(cat K2_${freq}MHz.dat | awk '{print $1}' ))
+    for f in ${files[@]}
+    do
+	base=${f%%.fits}
+	image=${base}_warped.fits
+	table=${base}_warped_comp.fits
+	bkg=${base}_bkg.fits
+	rms=${base}_rms.fits
+
+	if [[ ! -e ${table} ]]
+	then
+	    aegean ${image} --background ${bkg} --noise ${rms}\
+                   --table ${image} --priorized 1 \
+		   --input median_${freq}MHz_comp.fits --noregroup
+	fi
+    done
 done
