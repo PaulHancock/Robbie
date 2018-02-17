1965850ef8084727924c59a8abd8065e3b57a11c
PaulHancock <paul.hancock@curtin.edu.au>
2018-02-17 22:59:59 +0800

    update for new galaxy env

--- galaxy_codes/priorize_all.sh
+++ galaxy_codes/priorize_all.sh
@@ -1,11 +1,10 @@
 #! /bin/bash
 
-field=$1
+field="K2"
 
-nimg=(`ls HGL_${field}/1*warped.fits | wc`)
-nimg=${nimg[0]}
+nimg=$( ls K2_trim_[0-9]*[0-9]_warped.fits | wc -l )
 
 echo ${field} ${nimg}
 
 sed s/NNN/${field}/g priorize_all.template | sed s/NIMG/${nimg}/g > priorize_${field}.sh
-echo "sbatch priorize_${field}.sh"
+echo @( sbatch priorize_${field}.sh )
