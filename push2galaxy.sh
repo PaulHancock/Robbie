f81b3bd6b26e0811dcd1aa0069da392e6702f96b
PaulHancock <paul.hancock@curtin.edu.au>
2018-02-21 14:40:16 +0800

    fix typo

--- push2galaxy.sh
+++ push2galaxy.sh
@@ -6,4 +6,4 @@ rms=($( cat K2_1??MHz.dat | awk '{print $1}' | sed 's/.fits/_rms.fits/g' ))
 xmfiles=($( cat K2_1??MHz.dat | awk '{print $1}' | sed 's/.fits/_xm.fits/g' ))
 
 rsync --ignore-existing --progress ${files[@]} ${xmfiles[@]} ${bkg[@]} ${rms[@]} galaxy:/astro/mwasci/phancock/warping/.
-rsync --progress 'K2_1??MHz.dat' galaxy:/astro/mwasci/phancock/warping/.
+rsync --progress K2_1??MHz.dat galaxy:/astro/mwasci/phancock/warping/.
