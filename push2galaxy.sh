098e118094c40da80ce3a058eda24dd0ca4d53f3
PaulHancock <paul.hancock@curtin.edu.au>
2018-02-21 14:37:59 +0800

    also copy bkg/rms and K2_1??MHz.dat files

--- push2galaxy.sh
+++ push2galaxy.sh
@@ -1,7 +1,9 @@
 #! /usr/bin/env bash
 
 files=($( cat K2_1??MHz.dat | awk '{print $1}' ))
+bkg=($( cat K2_1??MHz.dat | awk '{print $1}' | sed 's/.fits/_bkg.fits/g'  ))
+rms=($( cat K2_1??MHz.dat | awk '{print $1}' | sed 's/.fits/_rms.fits/g' ))
 xmfiles=($( cat K2_1??MHz.dat | awk '{print $1}' | sed 's/.fits/_xm.fits/g' ))
 
-rsync --ignore-existing --progress ${files[@]} galaxy:/astro/mwasci/phancock/warping/.
-rsync --ignore-existing --progress ${xmfiles[@]} galaxy:/astro/mwasci/phancock/warping/.
+rsync --ignore-existing --progress ${files[@]} ${xmfiles[@]} ${bkg[@]} ${rms[@]} galaxy:/astro/mwasci/phancock/warping/.
+rsync --progress 'K2_1??MHz.dat' galaxy:/astro/mwasci/phancock/warping/.
