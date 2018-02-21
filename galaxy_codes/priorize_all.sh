faa0319790af45b6886086b76afcb73de4761f80
PaulHancock <paul.hancock@curtin.edu.au>
2018-02-21 14:33:52 +0800

    fix bug in script runner

--- galaxy_codes/priorize_all.sh
+++ galaxy_codes/priorize_all.sh
@@ -7,4 +7,4 @@ nimg=$( ls K2_trim_[0-9]*[0-9]_warped.fits | wc -l )
 echo ${field} ${nimg}
 
 sed s/NNN/${field}/g priorize_all.template | sed s/NIMG/${nimg}/g > priorize_${field}.sh
-echo @( sbatch priorize_${field}.sh )
+echo $( sbatch priorize_${field}.sh )
