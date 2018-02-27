f337c6a077508c0de4e2155417085aaa656c54b5
PaulHancock <paul.hancock@curtin.edu.au>
2018-02-27 13:33:22 +0800

    also push codes

--- push2galaxy.sh
+++ push2galaxy.sh
@@ -8,3 +8,4 @@ xmfiles=($( cat K2_1??MHz.dat | awk '{print $1}' | sed 's/.fits/_xm.fits/g' ))
 rsync --ignore-existing --progress ${files[@]} ${xmfiles[@]} ${bkg[@]} ${rms[@]} galaxy:/astro/mwasci/phancock/warping/.
 rsync --ignore-existing --progress K2_1??MHz.dat galaxy:/astro/mwasci/phancock/warping/.
 rsync --ignore-existing --progress median_{154,185}MHz{,_bkg,_rms,_comp}.fits galaxy:/astro/mwasci/phancock/warping/.
+rsync --progress galaxy_codes/* galaxy:/astro/mwasci/phancock/warping/.
