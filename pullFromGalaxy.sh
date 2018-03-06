a0fafaa0eef174de414661d1a6d686b40bc8a7bb
PaulHancock <paul.hancock@curtin.edu.au>
2018-03-06 16:53:19 +0800

    pull .reg files also

--- pullFromGalaxy.sh
+++ pullFromGalaxy.sh
@@ -1,3 +1,4 @@
 #! /usr/bin/env bash
 
-rsync --ignore-existing --progress galaxy:'/astro/mwasci/phancock/warping/*_warped{,_comp}.fits' .
+rsync --ignore-existing --progress galaxy:'/astro/mwasci/phancock/warping/*_warped.fits' .
+rsync --ignore-existing --progress galaxy:'/astro/mwasci/phancock/warping/*_warped_comp.{fits,reg}' .
