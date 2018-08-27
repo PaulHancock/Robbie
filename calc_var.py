b708558eb4f0f9fcd2d8dde32b0ec261fabc1f57
PaulHancock <paul.hancock@curtin.edu.au>
2018-08-27 21:18:12 +0800

    add variability plot to output

--- calc_var.py
+++ calc_var.py
@@ -71,7 +71,7 @@ def load_corrected_table(filename):
     return df
 
 
-def add_stats(df, outfile=None, ndof=None):
+def add_stats(df, outfile=None, ndof=None, plotfile=None):
     """
 
     Parameters
@@ -111,6 +111,36 @@ def add_stats(df, outfile=None, ndof=None):
 
     tab2 = Table.from_pandas(df)
     tab2.write(outfile, overwrite=True)
+    if plotfile is not None:
+        plot(tab2, plotfile)
+
+
+def plot(tab, plotfile):
+    """
+    Plot
+    """
+    import matplotlib
+    from matplotlib import pyplot
+
+    kwargs = {'fontsize':14}
+    fig = pyplot.figure(figsize=(5,8))
+
+    ax = fig.add_subplot(1,1,1)
+    cax = ax.scatter(tab['md'], np.log10(tab['pval_peak_flux']), c = np.log10(tab['peak_flux_1']))
+    cb = fig.colorbar(cax,ax=ax)
+    cb.set_label("log10(Peak flux in epoch 1) (Jy)", **kwargs)
+
+    ax.set_ylim((-11,1.001))
+    ax.set_xlim((-0.3,0.3))
+    ax.set_ylabel("log(p_val)", **kwargs)
+    ax.set_xlabel("Debiased modulation index ($m_d$)", **kwargs)
+    ax.axhline(-3, c='k')
+    ax.axvline(0.05, c='k')
+    ax.text(0.1, -5, "variable", **kwargs)
+    ax.fill_between([-0.3,0.05],-25, y2=2, color='k', alpha=0.2)
+    ax.fill_betweenx([-3,2],0.05, x2=0.3, color='k', alpha=0.2)
+    ax.text(-0.25, -5, "not variable", **kwargs)
+    pyplot.savefig(plotfile)
 
 
 if __name__ == '__main__':
@@ -122,6 +152,8 @@ if __name__ == '__main__':
                         help="Output catalogue")
     group1.add_argument("--ndof", dest='ndof', type=float, default=None,
                         help="Effective number of degrees of freedom. Defualt: N=epochs-1")
+    group1.add_argument("--plot", dest='plotfile', type=str, default=None,
+                        help="output plot")
 
     results = parser.parse_args()
 
@@ -130,5 +162,5 @@ if __name__ == '__main__':
         sys.exit()
 
     df = load_corrected_table(results.infile)
-    add_stats(df, results.outfile, ndof=results.ndof)
+    add_stats(df, results.outfile, ndof=results.ndof, plotfile=results.plotfile)
 
