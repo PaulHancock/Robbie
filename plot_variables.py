e8e65226f537788c298529d7da19379e13ec92b5
Paul Hancock <Paul.Hancock@curtin.edu.au>
2019-11-21 15:40:53 +0800

    plot from tables

--- plot_variables.py
+++ plot_variables.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 from __future__ import print_function
 
+from astropy.table import Table
 import dateutil
 import dateutil.parser
 import numpy as np
@@ -101,28 +102,114 @@ def plot_lc(cur, dates=False):
         print(" ... done")
     return
 
+def plot_summary_table(filename, plotfile):
+    """
+    Create a summary plot for all sources, identifying which are likely to be variable.
+
+    parameters
+    ----------
+    filename : str
+        Input table filename
+    plotfile : str
+        Filename for the output plot file
+    """
+    tab = Table.read(filename)
+    pval_peak_flux = tab['pval_peak_flux']
+    md = tab['md']
+    mean_peak_flux = tab['mean_peak_flux']
+
+    kwargs = {'fontsize':14}
+    fig = pyplot.figure(figsize=(5,8))
+
+
+    ax = fig.add_subplot(1,1,1)
+    cax = ax.scatter(md, np.log10(pval_peak_flux), c = np.log10(mean_peak_flux), cmap=matplotlib.cm.viridis_r)
+    cb = fig.colorbar(cax,ax=ax)
+    cb.set_label("log10(Peak flux in epoch 1) (Jy)", **kwargs)
+
+    ax.set_ylim((-11,1.001))
+    ax.set_xlim((-0.3,0.3))
+    ax.set_ylabel("log(p_val)", **kwargs)
+    ax.set_xlabel("Debiased modulation index ($m_d$)", **kwargs)
+    ax.axhline(-3, c='k')
+    ax.axvline(0.05, c='k')
+    ax.text(0.1, -5, "variable", **kwargs)
+    ax.fill_between([-0.3,0.05],-25, y2=2, color='k', alpha=0.2)
+    ax.fill_betweenx([-3,2],0.05, x2=0.3, color='k', alpha=0.2)
+    ax.text(-0.25, -5, "not variable", **kwargs)
+    pyplot.savefig(plotfile)
+    return
+
+def plot_lc_table(flux_table, stats_table):
+    """
+    Create individual light curve plots.
+    Each plot is saved to plots/uuid.png
+
+    parameters
+    ----------
+    filename : str
+        Filename of the flux table
+    """
+    ftab = Table.read(flux_table)
+    stab = Table.read(stats_table)
+    fluxes = [a for a in ftab.colnames if a.startswith('peak_flux')]
+    err_fluxes = [a for a in ftab.colnames if a.startswith('err_peak_flux')]
+    epoch = list(range(len(fluxes)))
+    for row in ftab:
+        fname = 'plots/{0}.png'.format(row['uuid'])
+        print(fname, end='')
+        if os.path.exists(fname):
+            print(" ... skip")
+            continue
+        srow = stab[stab['uuid'] == row['uuid']]
+
+        pyplot.clf()
+        s = 'm={0:5.3f}\nmd={1:4.2f}\nchisq={2:4.1f}'.format(
+             srow['m'][0], srow['md'][0], srow['chisq_peak_flux'][0])
+        pyplot.errorbar(epoch, list(row[fluxes]), yerr=list(row[err_fluxes]), label=s)
+        pyplot.ylabel('Flux Density (Jy/Beam)')
+        pyplot.xlabel('Epoch')
+        pyplot.title('{0}'.format(row['uuid']))
+        pyplot.legend()
+        pyplot.savefig(fname)
+        print(" ... done")
+    return
+       
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     group1 = parser.add_argument_group("Create a variability plot")
-    group1.add_argument("--name", dest='name', type=str, default=None,
-                        help="The input catalogue.")
+    group1.add_argument("--dbname", dest='db', type=str, default=None,
+                        help="The input database")
+    group1.add_argument("--ftable", dest='ftable', type=str, default=None,
+                        help="flux table")
+    group1.add_argument("--stable", dest='stable', type=str, default=None,
+                        help="stats table")
     group1.add_argument("--plot", dest='plotfile', type=str, default=None,
                         help="output plot")
     group1.add_argument("--all", dest='all', action='store_true', default=False,
                         help="Also plot individual light curves. Default:False")
-    group1.add_argument("--dates", dest='dates', action='store_true', default=False,
-                        help="Individual plots have date on the horizontal axis.")
+#    group1.add_argument("--dates", dest='dates', action='store_true', default=False,
+#                        help="Individual plots have date on the horizontal axis.")
 
     results = parser.parse_args()
 
-    if None in (results.name, results.plotfile):
+
+    if results.db:
+        conn = sqlite3.connect(results.name)
+        cur = conn.cursor()
+        plot_summary(cur=cur, plotfile=results.plotfile)
+        if results.all:
+            plot_lc(cur=cur, dates=results.dates)
+        conn.close()
+    elif results.ftable or results.stable:
+        if not (results.ftable and results.stable):
+            print("ERROR: --stable and --ftable are both required, only one supplied.")
+        plot_summary_table(results.stable, results.plotfile)
+        if results.all:
+            plot_lc_table(results.ftable, results.stable)
+    else:
         parser.print_help()
         sys.exit()
 
-    conn = sqlite3.connect(results.name)
-    cur = conn.cursor()
-    plot_summary(cur=cur, plotfile=results.plotfile)
-    if results.all:
-        plot_lc(cur=cur, dates=results.dates)
-    conn.close()
