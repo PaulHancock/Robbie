b0c4b7d49dbf408471f075b0dccc1525d4ffd268
PaulHancock <paul.hancock@curtin.edu.au>
2020-03-03 13:39:02 +0800

    get epoch from fits header easier than installing/requiring wcstools

---
+++ get_epoch.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+from __future__ import print_function
+
+from astropy.io import fits
+import argparse
+
+
+def get_obs_date(fname):
+    """
+    Get the observation date from the fits header of a given file
+    """
+    return fits.getheader(fname)['DATE-OBS']
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file", type=str,
+                        help="Filename (fits image)")
+    results = parser.parse_args()
+
+
+    print(get_obs_date(results.file))
