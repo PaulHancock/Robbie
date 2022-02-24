deddf23e1f131f4921c98067f6d0a6c3fabe3e77
PaulHancock <paul.hancock@curtin.edu.au>
2022-02-24 15:36:52 +0800

    use munlink if available

--- clean.sh
+++ clean.sh
@@ -1,7 +1,11 @@
 #! /usr/bin/env bash
 
 # delete the files and symlinks (but don't follow links)
-find -P work/?? -type f -print0 -o -type l -print0 | xargs -0 munlink 2&>/dev/null
+if [[ $(which munlink) ]]; then
+    find -P work/?? -type f -print0 -o -type l -print0 | xargs -0 munlink 2&>/dev/null
+else
+    find -P work/?? -type f -delete -o -type l -delete
+fi
 
 # delete the empty directories
 find -P work/?? -type d -empty -delete
