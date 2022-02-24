66b4c5fcb498506e4f3d5e1792e92f123f96ba03
PaulHancock <paul.hancock@curtin.edu.au>
2022-02-24 16:52:04 +0800

    use full repo name for dockerhub

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -1,8 +1,8 @@
 #! /usr/bin/env bash
 
-version=$(grep "^version" ../main.nf | awk '{print $3}' | sed s:\'::g )
+version=$(grep "^version" ../main.nf | awk '{print $3}' | sed s:\"::g )
 
 # build the container using
-docker build . -t "robbie-next:${version}"
+docker build . -t "paulhancock/robbie-next:${version}"
 # tag this build as latest
-docker tag robbie-next:${version} robbie-next:latest
+docker tag paulhancock/robbie-next:${version} paulhancock/robbie-next:latest
