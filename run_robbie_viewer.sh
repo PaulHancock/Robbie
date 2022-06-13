fa326a3505dd1d2ace5d155447a521426df0bb13
cjproud <christian.proud@curtin.edu.au>
2022-06-13 13:20:45 +0800

    Added -it flag to docker run to ensure graceful shutdown.

--- run_robbie_viewer.sh
+++ run_robbie_viewer.sh
@@ -6,7 +6,7 @@ if [ $1 ]
 then
     # Convert path to absolute path
     abs_path=$(readlink -f $1)
-    docker run -v $abs_path:/data -p 5006:5006 robbie/robbie-viewer:latest
+    docker run -it -v $abs_path:/data -p 5006:5006 -it robbie/robbie-viewer:latest
 else
-    docker run -v ${PWD}/results:/data -p 5006:5006 robbie/robbie-viewer:latest
+    docker run -it -v ${PWD}/results:/data -p 5006:5006 -it robbie/robbie-viewer:latest
 fi
