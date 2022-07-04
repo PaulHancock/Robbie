1f2769fcef9ca580aab1dfb87166a40ca7eb564c
Nick Swainston <nickaswainston@gmail.com>
2022-07-04 15:58:05 +0800

    Cleaned up the run_robbie_viewer.sh to avoid repetition

--- run_robbie_viewer.sh
+++ run_robbie_viewer.sh
@@ -1,22 +1,23 @@
 #! /usr/bin/env bash
 
 # Echo usage if something isn't right.
-usage() { 
+usage() {
     echo "
     Usage: $0 [-c <ra,dec,pad>] [-p <path_to_results> ]
 
     where:
         -c centre position of image in RA and DEC with a padding value in degrees around the centre. For example: -c 330,-20,5
         -p path to the results directory
-    " 1>&2; exit 1; 
+    " 1>&2; exit 1;
 }
 
 while getopts ":c:p:h" opt; do
-case ${opt} in 
+case ${opt} in
     c )
-        IFS=',' array=($OPTARG)
+        #IFS=',' array=($OPTARG)
+        array=$OPTARG
         ;;
-    p ) 
+    p )
         FILEPATH=$OPTARG
         ;;
     h)
@@ -30,26 +31,17 @@ case ${opt} in
     esac
 done
 
-# If coordinates and filepath
-if ! [ -z "${array}" ] && ! [ -z "${FILEPATH}" ]; then
-    echo "Specifying results path and RA, DEC and padding for cutout."
-    # Convert path to absolute path
+if [ -z "${FILEPATH}" ]; then
+    abs_path=${PWD}
+else
     abs_path=$(readlink -f ${FILEPATH})
-    docker run -it -v $abs_path:/data -p 5006:5006 robbie/robbie-viewer:latest bokeh serve . --args /data ${array[0]} ${array[1]} ${array[2]}
-
-# If only coordinates
-elif  ! [ -z "${array}" ] && [ -z "${FILEPATH}" ]; then
-    echo "Specifying only RA, DEC and padding for cutout."  
-    docker run -it -v ${PWD}/results:/data -p 5006:5006 robbie/robbie-viewer:latest bokeh serve . --args /data ${array[0]} ${array[1]} ${array[2]}
-
-# If only filepath
-elif   [ -z "${array}" ] && ! [ -z "${FILEPATH}" ]; then
-    # Convert path to absolute path
-    abs_path=$(readlink -f ${FILEPATH})
-    docker run -it -v $abs_path:/data -p 5006:5006 robbie/robbie-viewer:latest bokeh serve . --args /data
-
-# If neither
-elif  [ -z "${array}" ] && [ -z "${FILEPATH}" ]; then
-    echo "Using defaults"
-    docker run -it -v ${PWD}/results:/data -p 5006:5006 robbie/robbie-viewer:latest bokeh serve . --args /data 
 fi
+
+if [ -z "${array}" ]; then
+    pos_command=""
+else
+    pos_command=${array}
+fi
+
+echo docker run -it -v $abs_path:/data -p 5006:5006 robbie/robbie-viewer:latest bokeh serve . --args /data ${pos_command}
+docker run -it -v $abs_path:/data -p 5006:5006 robbie/robbie-viewer:latest bokeh serve . --args /data ${pos_command}
