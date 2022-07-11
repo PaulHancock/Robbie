816304cc7222656e45f7b4bae9daad6776c59191
cjproud <christian.proud@curtin.edu.au>
2022-07-11 15:53:06 +0800

    Change to docker build tag to support push.

--- robbie_viewer_server/build_docker.sh
+++ robbie_viewer_server/build_docker.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 
 # build the container and add a target
-docker build . -f ./Dockerfile -t "robbie/robbie-viewer"
+docker build . -f ./Dockerfile -t "cjproud/robbie_viewer:latest"
