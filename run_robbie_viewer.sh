1b50f9219a7bbf63de7b839e4b5decb7153f053b
PaulHancock <paul.hancock@curtin.edu.au>
2022-07-12 15:39:52 +0800

    change container root name to paulhancock

--- run_robbie_viewer.sh
+++ run_robbie_viewer.sh
@@ -6,7 +6,7 @@ if [ $1 ]
 then
     # Convert path to absolute path
     abs_path=$(readlink -f $1)
-    docker run -it -v $abs_path:/data -p 5006:5006 -it robbie/robbie-viewer:latest
+    docker run -it -v $abs_path:/data -p 5006:5006 -it paulhancock/robbie-viewer:latest
 else
-    docker run -it -v ${PWD}/results:/data -p 5006:5006 -it robbie/robbie-viewer:latest
+    docker run -it -v ${PWD}/results:/data -p 5006:5006 -it paulhancock/robbie-viewer:latest
 fi
